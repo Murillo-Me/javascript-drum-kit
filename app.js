dc589ca41a2feea314fe669c7210f5c91bb2f33d
Murillo Mendes <murillomendes95@hotmail.com>
2021-09-13 20:02:05 -0300

    Made it work by also clicking the div's

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ window.addEventListener('keydown', (e) => {
     })
     
   })
+
+  const drumBtn = document.querySelectorAll('.key')
+  drumBtn.forEach(btn => btn.addEventListener('click', (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.currentTarget.getAttribute('data-key')}"]`)
+    if (!audio) return;
+  
+  
+    audio.currentTime = 0
+    audio.play()
+
+    e.currentTarget.classList.add('playing')
+  
+    e.currentTarget.addEventListener('transitionend', (e) => {
+      if (e.propertyName !== 'transform') return;
+      e.target.classList.remove('playing')
+    })
+  }))
